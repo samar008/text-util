d7f3dcb9aa28917a22710aa780d7210e030d5687
samar008 <samarsharma008@gmail.com>
2022-10-10 07:35:02 +0530

    Updates

---
+++ precache-manifest.b1b8ef0178698fe8e0a54e65c03cd198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ab2e98933ba997f6343",
+    "url": "/text-util/static/css/main.c7cf8c01.chunk.css"
+  },
+  {
+    "revision": "2ab2e98933ba997f6343",
+    "url": "/text-util/static/js/main.2ab2e989.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/text-util/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "9a596575b6018fcea957",
+    "url": "/text-util/static/js/2.9a596575.chunk.js"
+  },
+  {
+    "revision": "1ce4e4c6158f0323f954",
+    "url": "/text-util/static/js/runtime~main.1ce4e4c6.js"
+  },
+  {
+    "revision": "a9e64ac5eaa178de99fe068f4e3adfa6",
+    "url": "/text-util/index.html"
+  }
+];
